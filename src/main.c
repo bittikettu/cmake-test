b6e349e5e2447e37062a84efb660a20b829e5b93
bittikettu <viidakkovekara@gmail.com>
2025-01-03 12:03:21 +0200

    raylib and clay tests

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <cellar/cellar.h>
 #include <stdint.h>
 #include <version.h>
-
+#include <raylib.h>
 // #include <git_hashes.h>
 // Ei oikein toimi jostain syystä.
 #include <jansson.h>
@@ -54,24 +54,92 @@ int janssoni(void) {
 	return 0;
 }
 
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"	 // Required for GUI controls
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
 int main(void) {
-	printf("Ver: %d.%d.%d\n", PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR, PROJECT_VERSION_PATCH);
-	janssoni();
-	int sum = calc_add(2, 34);
-	printf("sum %d\n", sum);
+	// Initialization
+	//--------------------------------------------------------------------------------------
+	const int screenWidth = 800;
+	const int screenHeight = 450;
 
-	BicycleCellar cellar;
+	InitWindow(screenWidth, screenHeight, "raylib [shapes] example - draw ring");
 
-	cellar_initialize(&cellar);
-	cellar_add_bicycle(&cellar, 1, "Alice");
-	cellar_add_bicycle(&cellar, 2, "Bob");
-	cellar_add_bicycle(&cellar, 3, "Charlie");
+	Vector2 center = {(GetScreenWidth() - 300) / 2.0f, GetScreenHeight() / 2.0f};
 
-	cellar_print_bicycle_cellar(&cellar);
+	float innerRadius = 80.0f;
+	float outerRadius = 190.0f;
 
-	cellar_mark_bicycle_in_use(&cellar, 2, true);
-	cellar_print_bicycle_cellar(&cellar);
+	float startAngle = 0.0f;
+	float endAngle = 360.0f;
+	float segments = 0.0f;
 
-	cellar_remove_bicycle(&cellar, 1);
-	cellar_print_bicycle_cellar(&cellar);
+	bool drawRing = true;
+	bool drawRingLines = false;
+	bool drawCircleLines = false;
+
+	SetTargetFPS(60);  // Set our game to run at 60 frames-per-second
+	//--------------------------------------------------------------------------------------
+
+	// Main game loop
+	while (!WindowShouldClose())  // Detect window close button or ESC key
+	{
+		// Update
+		//----------------------------------------------------------------------------------
+		// NOTE: All variables update happens inside GUI control functions
+		//----------------------------------------------------------------------------------
+
+		// Draw
+		//----------------------------------------------------------------------------------
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		DrawLine(500, 0, 500, GetScreenHeight(), Fade(LIGHTGRAY, 0.6f));
+		DrawRectangle(500, 0, GetScreenWidth() - 500, GetScreenHeight(), Fade(LIGHTGRAY, 0.3f));
+
+		if (drawRing)
+			DrawRing(center, innerRadius, outerRadius, startAngle, endAngle, (int) segments, Fade(MAROON, 0.3f));
+		if (drawRingLines)
+			DrawRingLines(center, innerRadius, outerRadius, startAngle, endAngle, (int) segments, Fade(BLACK, 0.4f));
+		if (drawCircleLines)
+			DrawCircleSectorLines(center, outerRadius, startAngle, endAngle, (int) segments, Fade(BLACK, 0.4f));
+
+		// Draw GUI controls
+		//------------------------------------------------------------------------------
+		GuiSliderBar((Rectangle) {600, 40, 120, 20}, "StartAngle", TextFormat("%.2f", startAngle), &startAngle, -450,
+					 450);
+		GuiSliderBar((Rectangle) {600, 70, 120, 20}, "EndAngle", TextFormat("%.2f", endAngle), &endAngle, -450, 450);
+
+		GuiSliderBar((Rectangle) {600, 140, 120, 20}, "InnerRadius", TextFormat("%.2f", innerRadius), &innerRadius, 0,
+					 100);
+		GuiSliderBar((Rectangle) {600, 170, 120, 20}, "OuterRadius", TextFormat("%.2f", outerRadius), &outerRadius, 0,
+					 200);
+
+		GuiSliderBar((Rectangle) {600, 240, 120, 20}, "Segments", TextFormat("%.2f", segments), &segments, 0, 100);
+
+		GuiCheckBox((Rectangle) {600, 320, 20, 20}, "Draw Ring", &drawRing);
+		GuiCheckBox((Rectangle) {600, 350, 20, 20}, "Draw RingLines", &drawRingLines);
+		GuiCheckBox((Rectangle) {600, 380, 20, 20}, "Draw CircleLines", &drawCircleLines);
+		//------------------------------------------------------------------------------
+
+		int minSegments = (int) ceilf((endAngle - startAngle) / 90);
+		DrawText(TextFormat("MODE: %s", (segments >= minSegments) ? "MANUAL" : "AUTO"), 600, 270, 10,
+				 (segments >= minSegments) ? MAROON : DARKGRAY);
+
+		DrawFPS(10, 10);
+
+		EndDrawing();
+		//----------------------------------------------------------------------------------
+	}
+
+	// De-Initialization
+	//--------------------------------------------------------------------------------------
+	CloseWindow();	// Close window and OpenGL context
+	//--------------------------------------------------------------------------------------
+
+	return 0;
 }
